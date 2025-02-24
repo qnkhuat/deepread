2686710c76f60e8e5fc955dc92c222184377ba67
Ngoc Khuat <qn.khuat@gmail.com>
2025-02-24 23:19:19 +0700

    man the electron just works

---
+++ app/main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
+
+let mainWindow;
+let backendProcess;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // In development, load from React dev server
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../frontend/dist/index.html'));
+  }
+}
+
+function startBackend() {
+  const backendPath = path.join(__dirname, '../backend/main.js');
+  backendProcess = spawn('node', [backendPath], {
+    stdio: 'inherit'
+  });
+
+  backendProcess.on('error', (error) => {
+    console.error('Failed to start backend process:', error);
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  startBackend();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    if (backendProcess) {
+      backendProcess.kill();
+    }
+    app.quit();
+  }
+});
+
+app.on('before-quit', () => {
+  if (backendProcess) {
+    backendProcess.kill();
+  }
+}); 
